4ff7c763459a9456f5858534ad8c373d2ac2bed4
gbromley <gbromley@wisc.edu>
2016-07-23 15:38:33 -0500

    added data directories

--- case_selection.py
+++ case_selection.py
@@ -1,4 +1,11 @@
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 import scipy
 import scipy.io as sio
+import netCDF4
+import os
+
+snow_data = '/data/SnowCover/data/'
+track_data = '/data/SnowCover/tracks/'
+
+snow_files = os.listdir(snow_data)
